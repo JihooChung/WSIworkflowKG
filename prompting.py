b279e847b1ebdce0d75cf473eb03d06a8f74b55f
JihooChung <105716418+JihooChung@users.noreply.github.com>
2026-06-05 10:57:36 +0200

    Create prompting.py

---
+++ prompting.py
@@ -0,0 +1,43 @@
+import json
+import requests
+
+url = "https://chat-ai.academiccloud.de/v1/chat/completions"
+api_key = 'API_KEY'
+
+prompt = 'PROMPT'
+input = 'INPUT'
+
+headers = {
+    "Accept": "application/json",
+    "Authorization": 'Bearer '+api_key,
+    "Content-Type": "application/json",
+    "inference-service": "saia-openai-gateway",
+}
+
+data = {
+    "model": "qwen3-30b-a3b-instruct-2507",
+    "messages": [
+        {"role": "system", "content": prompt},
+        {"role": "user", "content": input},
+    ],
+    "enable-tools": True,
+    "arcana": {
+        "id": "jihoo.chung01/nthpda"
+    },
+    "temperature": 0.0,
+    "top_p": 0.05,
+}
+
+
+try:
+    response = requests.post(url, headers=headers, json=data)
+    response.raise_for_status()
+
+    result = response.json()
+    print(result["choices"][0]["message"]["content"])
+
+except requests.exceptions.HTTPError as http_err:
+    print(f"HTTP Error: {http_err}")
+    print(f"{response.text}")
+except Exception as err:
+    print(f"Error: {err}")
